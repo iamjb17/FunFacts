6a09f131ef1ecd3f5bd269c9b56b6a494e32ca95
Jessie B <iamjb05@gmail.com>
2017-11-19 15:40:12 -0600

    Update FunFactsActivity.java

--- app/src/main/java/com/example/jessie/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/jessie/funfacts/FunFactsActivity.java
@@ -23,21 +23,19 @@ public class FunFactsActivity extends AppCompatActivity {
     private TextView mFactTextView;
     private Button mShowFactButton;
     private ConstraintLayout mConstraintLayout;
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fun_facts);
+        setContentView(R.layout.activity_fun_facts);        
+        
         /* Assign the views from the layout file to the corresponding
          variable(connect views to variables)- We initialize our view variables to
          the views in the layout by using their id's
           */
-
         mFactTextView = (TextView) findViewById(R.id.factText);
         mShowFactButton = (Button) findViewById(R.id.showFactButton);
         mConstraintLayout = (ConstraintLayout) findViewById(R.id.colorChange);
-
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -45,21 +43,14 @@ public class FunFactsActivity extends AppCompatActivity {
                 String fact = mFactBook.getFact();
                 int color = mColorWheel.getColor();
 
-
                 // Update the screen with our dynamic fact
                 mFactTextView.setText(fact);
                 mConstraintLayout.setBackgroundColor(color);
                 mShowFactButton.setTextColor(color);
-
-
             }
         };
         mShowFactButton.setOnClickListener(listener);
-
         // Toast.makeText(FunFactsActivity.this, "Yay our activity is created!", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "We are checking on the Log and checking a commit");
-
-
-
     }
 }
