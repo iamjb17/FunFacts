55f772125b177ea78625bb677882c50a2bfb942b
Jessie B <iamjb05@gmail.com>
2017-11-19 15:44:34 -0600

    Update ColorWheel.java

--- app/src/main/java/com/example/jessie/funfacts/ColorWheel.java
+++ app/src/main/java/com/example/jessie/funfacts/ColorWheel.java
@@ -1,7 +1,6 @@
 package com.example.jessie.funfacts;
 
 import android.graphics.Color;
-
 import java.util.Random;
 
 /**
@@ -10,6 +9,7 @@ import java.util.Random;
 
 public class ColorWheel {
     private String[] mColors = {
+        // the different colors that the app cycles through
             "#39add1", // light blue
             "#3079ab", // dark blue
             "#c25975", // mauve
@@ -27,17 +27,12 @@ public class ColorWheel {
 
     // Create methods- The acts the object can take.
     public int getColor() {
-
-
         String color;
         // Randomly select a color
         Random randomGenerator = new Random();
         int randomNumber = randomGenerator.nextInt(mColors.length);
         color = mColors[randomNumber];
         int colorAsInt = Color.parseColor(color);
-
         return colorAsInt;
-
     }
-
 }
